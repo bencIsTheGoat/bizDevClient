37539731309bf2e89884d7db80b881e10081ba83
Ben Cutler <bcutler94@gmail.com>
2020-07-31 16:00:53 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { VictoryPie } from 'victory'
+
+
 
 function App() {
+
+  const [pie, setPie] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/data?states=nj")
+        const { 
+          nj = []
+        } = await res.json();
+
+        const operators = nj[0].onlineRevTable[0].slice(1);
+        const rev = nj[0].onlineRevTable[2].slice(1);
+        
+        setPie(operators.map((op, idx) => {
+          return {
+            x: op,
+            y: rev[idx]
+          }
+        }))
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    getData()
+    // const data = getData();
+    // console.log(data)
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <VictoryPie
+        data={pie}
+        animate={true}
+        colorScale="qualitative"
+      />
     </div>
   );
 }
